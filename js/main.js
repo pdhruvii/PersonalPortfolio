3045d7ba622697eceb4dcfe2dbf8746462f1fd9c
Dhruvi Patel <Dhruvi24.patell@gmail.com>
2023-03-26 19:49:31 -0400

    Added Swiper JS Card Slider Skeletal

--- js/main.js
+++ js/main.js
@@ -18,3 +18,21 @@ window.onscroll = () => {
 
     })
 }
+
+var swiper = new Swiper(".extracurriculars-container", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 2,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
